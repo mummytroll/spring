e4b3725c61f463ae9253949d45d9a6310ddd9243
mummytroll <andron_d@mail.ru>
2022-06-07 16:01:35 +0300

    v2.1

---
+++ demo_m/build/src/main/java/com/example/demo_m/DemoMApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMApplication.class, args);
+    }
+
+}
